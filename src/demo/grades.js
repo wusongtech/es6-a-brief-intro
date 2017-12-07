b140c775ae291d3909873135a0ad3b78911997d0
wangshijiang <wangshijiang@wusongtech.com>
2017-12-07 10:27:29 +0800

    使用create-react-app创建demo项目

---
+++ src/demo/grades.js
@@ -0,0 +1,18 @@
+var grades = [
+    {name: 'John', grade: 60},
+    {name: 'Judy', grade: 100}
+];
+export function getGrades() {
+    return grades;
+}
+
+export default function foo() {
+    console.info('foo');
+}
+export function bar() {
+    console.info('bar');
+}
+export function baz() {
+    console.info('baz');
+}
+
